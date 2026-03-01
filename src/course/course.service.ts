32dc93c401dadc8144c5661c52d49b7d1e2878a9
riteshchaudhary <riteshchaudhary5191@gmail.com>
2026-03-01 11:20:59 +0530

    feat(course): enhance course module with Mongoose integration and validation

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '../user/schemas/userSchema';
+import { Model } from 'mongoose';
+import { Course } from './schemas/course.schema';
 
 @Injectable()
 export class CourseService {
-  create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
+  constructor(@InjectModel(Course.name) private courseModel: Model<Course>) {}
+  async create(createCourseDto: CreateCourseDto) {
+      return await this.courseModel.create({
+        name: createCourseDto.name,
+        description: createCourseDto.description,
+        level: createCourseDto.level,
+        price: createCourseDto.price,
+      });
   }
 
   findAll() {
--- src/course/dto/create-course.dto.ts
+++ src/course/dto/create-course.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCourseDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateCourseDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  level: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+}
